af579efdc32cbce38cc0513e116a4ebf9e9392a6
Shadowbee <169397554+Shadowbee27@users.noreply.github.com>
2025-03-27 17:51:58 +0100

    implemented game loop competed input_handler implemented clone for ActivePlayer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use std::io::*;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 enum TileState {
     Empty,
     PlayerX,
     PlayerO,
 }
+#[derive(PartialEq)]
 enum ActivePlayer {
     PlayerX,
     PlayerO,
 }
 
+impl Clone for ActivePlayer {
+    fn clone(&self) -> ActivePlayer {
+        match self {
+            ActivePlayer::PlayerX => ActivePlayer::PlayerX,
+            ActivePlayer::PlayerO => ActivePlayer::PlayerO,
+        }
+    }
+}
+
 fn main() {
-    println!("{}", input());
+    let player = ActivePlayer::PlayerX;
     let field: [TileState; 9] = [
         TileState::Empty,
         TileState::Empty,
@@ -24,32 +34,53 @@ fn main() {
         TileState::Empty,
         TileState::Empty,
     ];
-    print_board(field);
+    game(field, player)
 }
 fn input() -> String {
     let mut field = String::new();
     stdin().read_line(&mut field).expect("Error");
     field.trim().to_string()
 }
-fn input_handeler(input: String, board: [TileState; 9]) -> [TileState; 9] {
+fn input_handler(
+    input: String,
+    mut board: [TileState; 9],
+    mut player: ActivePlayer,
+) -> ([TileState; 9], ActivePlayer) {
     let mut change_field = 0;
-    match input {
-        String::from("a1") => change_field = 1,
-        String::from("a2") => change_field = 2,
-        String::from("a3") => change_field = 3,
-        String::from("b1") => change_field = 4,
-        String::from("b2") => change_field = 5,
-        String::from("b3") => change_field = 6,
-        String::from("c1") => change_field = 7,
-        String::from("c2") => change_field = 8,
-        String::from("c3") => change_field = 9,
+    match input.as_str() {
+        "a1" => change_field = 1,
+        "a2" => change_field = 2,
+        "a3" => change_field = 3,
+        "b1" => change_field = 4,
+        "b2" => change_field = 5,
+        "b3" => change_field = 6,
+        "c1" => change_field = 7,
+        "c2" => change_field = 8,
+        "c3" => change_field = 9,
         _ => panic!(),
     }
-    board
+    change_field -= 1;
+    if board[change_field] == TileState::PlayerX || board[change_field] == TileState::PlayerO {
+        println!("Field is already taken");
+        game(board.clone(), player.clone());
+    } else if player == ActivePlayer::PlayerX {
+        board[change_field] = TileState::PlayerX;
+        player = ActivePlayer::PlayerO
+    } else if player == ActivePlayer::PlayerO {
+        board[change_field] = TileState::PlayerO;
+        player = ActivePlayer::PlayerX
+    }
+    (board, player)
 }
-fn game(board: [TileState; 9]) {
-    todo!()
-}
+fn game(mut board: [TileState; 9], mut player: ActivePlayer) {
+    loop {
+    let field = input();
+    let new_board = input_handler(field, board, player);
+    board = new_board.0;
+    player = new_board.1;
+//    win_detection()
+    print_board(board.clone())
+}}
 
 fn print_board(field: [TileState; 9]) {
     let mut board = String::new();
@@ -66,7 +97,10 @@ fn print_board(field: [TileState; 9]) {
         } else if *t == TileState::PlayerO {
             board = format!("{board} O");
         }
-        counter = counter + 1;
+        counter += 1;
     }
     println!("{board}");
+}
+fn win_detection() {
+    todo!()
 }
